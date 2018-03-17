c5230a11cb3d2a514c0662c4a93036f9b3f400b5
Yuri Tveritin <kentyku@bk.ru>
2018-03-17 13:58:07 +0400

    добавил входной массив и передачу его значений в матрицу учел неполный виток учел форматирование матрицы для больших чисел

--- src/snakearray/SnakeArray.java
+++ src/snakearray/SnakeArray.java
@@ -5,9 +5,12 @@
  */
 package snakearray;
 
+import java.util.Scanner;
+
 /**
  *
- * @author user
+ * @author Yuri Tveritin
+ * @version 1.0
  */
 public class SnakeArray {
 
@@ -16,9 +19,21 @@ public class SnakeArray {
      */
     public static void main(String[] args) {
         
-        int N=8;
-        int [] ar=new int[N*N];
-        int[][] a=new int[N][N];
+        int n;
+        
+        System.out.println("Введите положительное целое число");
+        Scanner in = new Scanner(System.in);
+        n=in.nextInt();
+        int [] ar=new int[n*n];
+        for (int i=0;i<n*n;i++){
+            ar[i]=i+1;
+        }
+        
+        for (int i=0;i<n*n;i++){
+            System.out.print(ar[i]);
+        }
+        System.out.println();
+        int[][] a=new int[n][n];
         int count;
         int vitok;
         int gor1;
@@ -26,37 +41,43 @@ public class SnakeArray {
         int gor2;
         int vert2;
         int i,j;
+       
 //        System.out.println((N+1)/2);
-        count=1;
+        count=0;
 //          {цикл, который отвечает за количество витков спирали}
-        for (vitok=0;vitok<((N+1)/2);vitok++) {
+        for (vitok=0;vitok<(n/2);vitok++) {
         
 //          {цикл, верхней горизотальной части витка}
-            for(gor1=vitok;gor1<N-1-vitok;gor1++){
-                a[vitok][gor1]=count;
+            for(gor1=vitok;gor1<n-1-vitok;gor1++){
+                a[vitok][gor1]=ar[count];
                 count++;
             }
 //          {цикл, правой вертикальной части витка (той, что сверху вниз)}  
-            for(vert1=vitok;vert1<N-1-vitok;vert1++){
-                a[vert1][((N-1)-vitok)]=count;
+            for(vert1=vitok;vert1<n-1-vitok;vert1++){
+                a[vert1][((n-1)-vitok)]=ar[count];
                 count++;
             }
 //          {цикл, нижней горизонтальной части витка (той, что справа налево)}  
-          for (gor2=((N-1)-vitok); gor2>vitok;gor2--){//g3:=((N+1)-vitok) downto vitok+1 do
-              a[((N-1)-vitok)][gor2]=count;
+          for (gor2=((n-1)-vitok); gor2>vitok;gor2--){//g3:=((N+1)-vitok) downto vitok+1 do
+              a[((n-1)-vitok)][gor2]=ar[count];
               count++;
           }
 //          {цикл, левой вертикальной части витка (той, что снизу вверх)}  
-          for (vert2=(N-1-vitok);vert2>vitok;vert2--){//(g4:=((N+1)-vitok) downto vitok+1 do
-              a[vert2][vitok]=count;
+          for (vert2=(n-1-vitok);vert2>vitok;vert2--){//(g4:=((N+1)-vitok) downto vitok+1 do
+              a[vert2][vitok]=ar[count];
               count++;
           }
         }
+        
+         //учитываем неполный виток 
+        if(n%2==1){
+            a[(n/2)][n/2]=n*n;
+        }
      
-      for (i=0;i<N;i++){
+      for (i=0;i<n;i++){
       
-        for (j=0;j<N;j++){
-          System.out.print(a[i][j]);
+        for (j=0;j<n;j++){
+          System.out.printf("%"+(n/10+2)+"d",a[i][j]);
           System.out.print("  ");
         }
         System.out.println();
